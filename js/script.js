61b6880bb8b0ad039af8fb2b46a0bae6b81c5624
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-25 12:11:14 +0700

    membuat website company profile dengan materialize part 2

--- js/script.js
+++ js/script.js
@@ -8,5 +8,13 @@ M.Slider.init(slider,{
     interval: 3000
 });
 
-const parallax = document.querySelectorAll('.parallax')
-M.Parallax.init(parallax)
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
+
+const card = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(card);
+
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll, {
+    scrollOffset: 50
+});
